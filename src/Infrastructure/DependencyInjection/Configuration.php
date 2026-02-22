80d91c7409f975d62f0ba632b7639fab7ea5faf1
Arnaud Moncond'huy <arnaudmoncondhuy@gmail.com>
2026-02-22 13:11:23 +0000

    fix: Correct namespace for Configuration class
    Add Infrastructure segment to namespace for Configuration class.

--- src/Infrastructure/DependencyInjection/Configuration.php
+++ src/Infrastructure/DependencyInjection/Configuration.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace ArnaudMoncondhuy\SynapseBundle\DependencyInjection;
+namespace ArnaudMoncondhuy\SynapseBundle\Infrastructure\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
